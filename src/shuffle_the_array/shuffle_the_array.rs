b5d3076a4f5c8938303e09e8a56f9c0dc8ccffa4
Carson Rajcan <carajcan@gmail.com>
2020-09-26 11:26:17 -0500

    Run linter for first time

--- src/shuffle_the_array/shuffle_the_array.rs
+++ src/shuffle_the_array/shuffle_the_array.rs
@@ -9,15 +9,17 @@ pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
     res
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_shuffle() {
-       assert_eq!(shuffle(vec![1, 1, 2, 2],               2), [1, 2, 1, 2]);
-       assert_eq!(shuffle(vec![2, 5, 1, 3, 4, 7],         3), [2, 3, 5, 4, 1, 7]);
-       assert_eq!(shuffle(vec![1, 2, 3, 4, 4, 3, 2, 1, ], 4), [1, 4, 2, 3, 3, 2, 4, 1]);
+        assert_eq!(shuffle(vec![1, 1, 2, 2], 2), [1, 2, 1, 2]);
+        assert_eq!(shuffle(vec![2, 5, 1, 3, 4, 7], 3), [2, 3, 5, 4, 1, 7]);
+        assert_eq!(
+            shuffle(vec![1, 2, 3, 4, 4, 3, 2, 1,], 4),
+            [1, 4, 2, 3, 3, 2, 4, 1]
+        );
     }
 }
--- src/two_sum/two_sum.rs
+++ src/two_sum/two_sum.rs
@@ -1,7 +1,7 @@
 fn compare_with_rest(i: usize, num: i32, nums: &[i32], target: i32) -> Option<Vec<usize>> {
-    for (j, other_num) in nums[i+1..].iter().enumerate() {
+    for (j, other_num) in nums[i + 1..].iter().enumerate() {
         if num + other_num == target {
-            return Some(vec![i, (j + i+1)]);
+            return Some(vec![i, (j + i + 1)]);
         }
     }
 
@@ -10,9 +10,9 @@ fn compare_with_rest(i: usize, num: i32, nums: &[i32], target: i32) -> Option<Ve
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     for (i, num) in nums.iter().enumerate() {
-        match compare_with_rest(i, *num, nums.as_slice(), target){
+        match compare_with_rest(i, *num, nums.as_slice(), target) {
             Some(result) => return result.into_iter().map(|x| x as i32).collect(),
-            None         => ()
+            None => (),
         }
     }
 
@@ -20,13 +20,13 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
     fn test_two_sum() {
-        assert_eq!(two_sum(vec![2,7,11,15],    9), vec![0,1]);
-        assert_eq!(two_sum(vec![14,2,4,5,6,7], 8), vec![1,4]);
-        assert_eq!(two_sum(vec![3,2,4],        6), vec![1,2]);
+        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(two_sum(vec![14, 2, 4, 5, 6, 7], 8), vec![1, 4]);
+        assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
     }
 }
