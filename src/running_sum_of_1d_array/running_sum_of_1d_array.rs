067b8461a0d340fbbff433ef25ac2430ee356b49
Carson Rajcan <carajcan@gmail.com>
2020-09-27 17:36:39 -0500

    WIP

--- src/running_sum_of_1d_array/running_sum_of_1d_array.rs
+++ src/running_sum_of_1d_array/running_sum_of_1d_array.rs
@@ -9,22 +9,23 @@ pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
 
     res
 }
-
-fn running_sum_helper(nums: Vec<i32>, sum: i32) -> Vec<i32> {
-    match nums.as_slice() {
-        [] => vec![],
-        _ => {
+/*
                  println!("nums[0]: {:?}",   nums[0]);
                  println!("nums[1..]: {:?}", &nums[1..]);
+                 let () = &nums[1..];
                  vec![]
-             }
+fn running_sum_helper(nums: &[i32], sum: i32) -> &'static [i32] {
+    match nums {
+        [] => &[],
+        _ => &[nums[0]]
     } 
 }
 
 pub fn functional_running_sum(nums: Vec<i32>) -> Vec<i32> {
-    running_sum_helper(nums, 0)
+    running_sum_helper(nums.as_slice(), 0).to_vec()
 }
 
+*/
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,10 +35,11 @@ mod tests {
         assert_eq!(running_sum(vec![1, 1, 1, 1, 1]), [1, 2, 3, 4, 5]);
         assert_eq!(running_sum(vec![1, 2, 3, 4]), [1, 3, 6, 10]);
     }
-
+/*
     #[test]
     fn test_functional_running_sum() {
-//       assert_eq!(functional_running_sum(vec![1, 1, 1, 1, 1]), [1, 2, 3, 4, 5]);
-       assert_eq!(functional_running_sum(vec![1, 2, 3, 4]),    [1, 3, 6, 10]);
+        assert_eq!(functional_running_sum(vec![1, 1, 1, 1, 1]), [1, 2, 3, 4, 5]);
+         assert_eq!(functional_running_sum(vec![1, 2, 3, 4]),    [1, 3, 6, 10]);
     }
+*/
 }
