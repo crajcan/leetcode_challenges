2a176e2d3717df082f72e2b520283c0f8514edaf
Carson Rajcan <carajcan@gmail.com>
2020-10-07 23:47:03 -0500

    Green! sum_odd_length_subarrays

--- src/sum_odd_length_subarrays/sum_odd_length_subarrays.rs
+++ src/sum_odd_length_subarrays/sum_odd_length_subarrays.rs
@@ -1,14 +1,25 @@
 fn subarrays(arr: &[i32]) -> Vec<&[i32]> {
+    let mut res: Vec<&[i32]> = vec![];
 
+    for i in 0..arr.len() {
+        for j in i..arr.len() {
+            res.push(&arr[i..=j]);
+        }
+    }
 
-    vec![&arr[0..1], &arr[2..3]]
+    res
 }
 
-/*
 pub fn sum_odd_length_subarrays(arr: Vec<i32>) -> i32 {
-   arr[1] 
+    subarrays(arr.as_slice()).iter().fold(0, |acc, x| {
+        if x.len() % 2 == 0 {
+            acc
+        } else {
+            let sum: i32 = x.iter().sum();
+            acc + sum
+        }
+    })
 }
-*/
 
 #[cfg(test)]
 mod test {
@@ -16,41 +27,20 @@ mod test {
 
     #[test]
     fn test_subarrays() {
-        let arr = vec![1,2,3,4,5];
-        let res = subarrays(arr.as_slice());
+        let expected: Vec<&[i32]> = vec![];
+        assert_eq!(subarrays(&[]), expected);
 
-        println!("res: {:?}", res);
-/*
-        assert_eq!(subarrays(vec![1, 2]), vec![vec![1], vec![2]]);
-        assert_eq!(
-            subarrays(vec![10, 11, 12]),
-            vec![vec![10], vec![11], vec![12], vec![10, 11, 12]]
-        );
-        let arr = subarrays(vec![1, 4, 2, 5, 3]);
-        let slice = &[0..2];
+        let expected: Vec<&[i32]> = vec![&[1]];
+        assert_eq!(subarrays(&[1]), expected);
 
-        assert_eq!(
-            arr,
-            vec![
-                &arr[0],
-                &arr[1],
-                &arr[2],
-                &arr[3],
-                &arr[4],
-                &arr[0..2],
-                &arr[1..3],
-                &arr[2..4],
-                &[0..4]
-            ]
-        );
-*/
+        let expected: Vec<&[i32]> = vec![&[1], &[1, 2], &[1, 2, 3], &[2], &[2, 3], &[3]];
+        assert_eq!(subarrays(&[1, 2, 3]), expected);
     }
-/*
+
     #[test]
     fn test_sub_odd_legnth_subarrays() {
         assert_eq!(sum_odd_length_subarrays(vec![1, 2]), 3);
         assert_eq!(sum_odd_length_subarrays(vec![10, 11, 12]), 66);
         assert_eq!(sum_odd_length_subarrays(vec![1, 4, 2, 5, 3]), 58);
     }
-*/
 }
