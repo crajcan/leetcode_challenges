73c9297408059d888466da83f494314d0a500b2d
Carson Rajcan <carajcan@gmail.com>
2020-10-11 13:26:57 -0500

    Finish count good triplets

--- src/count_good_triplets/count_good_triplets.rs
+++ src/count_good_triplets/count_good_triplets.rs
@@ -1,35 +1,43 @@
 pub fn add_to_each(arr: Vec<Vec<i32>>, x: i32) -> Vec<Vec<i32>> {
-    arr.iter().map(|sub_array| [sub_array.to_vec(), vec![x]].concat()).collect()
+    arr.iter()
+        .map(|sub_array| [sub_array.to_vec(), vec![x]].concat())
+        .collect()
 }
 
 pub fn powerset(arr: &Vec<i32>) -> Vec<Vec<i32>> {
     match arr.split_last() {
-        None => { println!("*** No split.last, arr: {:?} ***", arr);
-                vec![vec![]] },
-        Some((x, xs)) => {
-            [
-                powerset(&xs.to_vec()),
-                add_to_each(powerset(&xs.to_vec()), *x) 
-            ].concat()
-        }
+        None => vec![vec![]],
+        Some((x, xs)) => [
+            powerset(&xs.to_vec()),
+            add_to_each(powerset(&xs.to_vec()), *x),
+        ]
+        .concat(),
     }
 }
 
 fn triplets(arr: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    arr.iter().fold(vec![], |acc, e| if e.len() == 3 { [acc, vec![e.to_vec()]].concat() } else { acc })
-}
-/*
-pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
-    (triplets(powerset(&arr))).iter().fold(0, |count, i| {
-        if ((arr[i] - arr[i + 1]).abs() <= a)
-            && ((arr[i + 1] - arr[i + 2]).abs() <= b)
-            && ((arr[i] - arr[i + 2]).abs() <= c)
-        {
-            count + 1
+    arr.iter().fold(vec![], |acc, e| {
+        if e.len() == 3 {
+            [acc, vec![e.to_vec()]].concat()
+        } else {
+            acc
         }
     })
 }
-*/
+
+pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
+    (triplets(powerset(&arr))).iter().fold(0, |count, triplet| {
+        if ((triplet[0] - triplet[1]).abs() <= a)
+            && ((triplet[1] - triplet[2]).abs() <= b)
+            && ((triplet[0] - triplet[2]).abs() <= c)
+        {
+            count + 1
+        } else {
+            count
+        }
+    })
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -49,27 +57,45 @@ mod test {
     #[test]
     fn test_powerset() {
         assert_eq!(powerset(&vec![1]), vec![vec![], vec![1]]);
-        assert_eq!(powerset(&vec![1, 2]), vec![vec![], vec![1], vec![2], vec![1, 2]]);
+        assert_eq!(
+            powerset(&vec![1, 2]),
+            vec![vec![], vec![1], vec![2], vec![1, 2]]
+        );
         assert_eq!(
             powerset(&vec![1, 2, 3]),
-            vec![vec![], vec![1], vec![2], vec![1, 2], vec![3], vec![1, 3], vec![2, 3], vec![1, 2, 3]]
+            vec![
+                vec![],
+                vec![1],
+                vec![2],
+                vec![1, 2],
+                vec![3],
+                vec![1, 3],
+                vec![2, 3],
+                vec![1, 2, 3]
+            ]
         );
     }
 
     #[test]
     fn test_triplets() {
         assert_eq!(
-            triplets(
-                vec![vec![], vec![1], vec![2], vec![1, 2], vec![3], vec![1, 3], vec![2, 3], vec![1, 2, 3]]
-            ),
+            triplets(vec![
+                vec![],
+                vec![1],
+                vec![2],
+                vec![1, 2],
+                vec![3],
+                vec![1, 3],
+                vec![2, 3],
+                vec![1, 2, 3]
+            ]),
             vec![vec![1, 2, 3]]
         )
     }
-/*
+
     #[test]
     fn test_count_good_triplets() {
         assert_eq!(count_good_triplets(vec![3, 0, 1, 1, 9, 7], 7, 2, 3), 4);
         assert_eq!(count_good_triplets(vec![1, 1, 2, 2, 3], 0, 0, 1), 0);
     }
-*/
 }
