7d56d040713b04ff59ed40df7241426a4c9286d3
Carson Rajcan <carajcan@gmail.com>
2020-10-19 23:09:17 -0500

    freq_alphabets

--- src/freq_alphabets/mod.rs
+++ src/freq_alphabets/mod.rs
@@ -1,20 +1,32 @@
 fn translate(s: &str) -> char {
     (96 + s.parse::<u8>().unwrap()) as char
 }
-/*
-pub fn freq_alphabets(s: String) -> String {
-    let i = s.len() - 1;
-    let mut result = String::new();
-    
 
+pub fn freq_alphabets(s: String) -> String {
+    let mut result = String::new();
+    let bytes = s.as_bytes();
+
+    let mut i = (s.len() - 1) as i32;
     while i >= 0 {
-        match s[i] {
-            '#' => result.insert(0, translate(&s[i-2..i])),
-            _ => result.insert(0, translate(&s[i-1..i]))
-       }
+        match bytes[i as usize] as char {
+            '#' => {
+                let mut sub = String::new();
+                sub.push(bytes[(i - 2) as usize] as char);
+                sub.push(bytes[(i - 1) as usize] as char);
+
+                result.insert(0, translate(&sub));
+                i -= 3;
+            }
+            _ => {
+                result.insert(0, translate(&(bytes[i as usize] as char).to_string()));
+                i -= 1;
+            }
+        }
     }
+
+    result
 }
-*/
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -27,10 +39,9 @@ mod test {
         assert_eq!(translate("26"), 'z');
     }
 
-/*
     #[test]
     fn test_freq_alphabets() {
-        assert_eq!(freq_alphabets("1236#".to_string()), "acz".to_string());
+        assert_eq!(freq_alphabets("1326#".to_string()), "acz".to_string());
         assert_eq!(freq_alphabets("10#11#12".to_string()), "jkab".to_string());
         assert_eq!(
             freq_alphabets(
@@ -39,5 +50,4 @@ mod test {
             "abcdefghijklmnopqrstuvwxyz".to_string()
         );
     }
-*/
 }
