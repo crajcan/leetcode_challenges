b7cbe77754c8477eac1438f44363befbd77e46a6
Carson Rajcan <carajcan@gmail.com>
2022-06-27 12:05:22 -0500

    Finish length of last word

---
+++ src/problems/length_of_last_word.rs
@@ -0,0 +1,14 @@
+pub fn length_of_last_word(s: String) -> i32 {
+    s.split_whitespace().last().unwrap().len() as i32
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_length_of_last_word() {
+        assert_eq!(length_of_last_word("Hello World".to_string()), 5);
+    }
+}
