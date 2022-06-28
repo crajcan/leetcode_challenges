55fc2dbf0af98c500ccf5118f1134ad2cd5476b4
Carson Rajcan <carajcan@gmail.com>
2022-06-28 13:06:22 -0500

    Implement O(log(n)) search insert

--- src/problems/easy/search_insert.rs
+++ src/problems/easy/search_insert.rs
@@ -1,21 +1,29 @@
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let len: i32 = nums.len() as i32;
+    use std::cmp::Ordering;
+    let mut lo = 0;
+    let mut hi = nums.len();
     
-    let res: i32 = nums.into_iter().enumerate().fold(-1, |acc, (i, j)| {
-            if target <= j && acc == -1 {
-                i as i32 
-            } else {
-                acc
-            }
+    while lo < hi {
+        let mid = lo + ((hi - lo) / 2) ;
+
+        match target.cmp(&nums[mid]) {
+            Ordering::Greater => lo = mid + 1,
+            Ordering::Less => hi = mid,
+            Ordering::Equal => return mid as i32,
         }
-    );
+    }
+    println!("lo: {}, hi: {}", lo, hi);
+    if lo == nums.len() {
+        return nums.len() as i32;
+    }
     
-    if res == -1 {
-        return len;
-    } 
-    
-    res
+    if target < nums[lo] as i32 {
+        lo as i32
+    } else {
+        (lo + 1) as i32
+    }
 }
+
 
 #[cfg(test)]
 mod test {
@@ -23,9 +31,9 @@ mod test {
 
     #[test]
     fn test_search_insert() {
-        assert_eq!(search_insert(vec![1,3,5,6], 5), 2);
-        assert_eq!(search_insert(vec![1,3,5,6], 2), 1);
-        assert_eq!(search_insert(vec![1,3,5,6], 7), 4);
-        assert_eq!(search_insert(vec![1,3,5,6], 0), 0);
+        assert_eq!(search_insert(vec![1, 3, 5, 6], 5), 2);
+        assert_eq!(search_insert(vec![1, 3, 5, 6], 2), 1);
+        assert_eq!(search_insert(vec![1, 3, 5, 6], 7), 4);
+        assert_eq!(search_insert(vec![1, 3, 5, 6], 0), 0);
     }
 }
