e18d67903235986113b0d204389ca42c12bdd55c
Carson Rajcan <carajcan@gmail.com>
2022-07-10 14:39:53 -0500

    Postorder traversal

---
+++ src/problems/easy/postorder_traversal.rs
@@ -0,0 +1,55 @@
+// #[derive(Debug, PartialEq, Eq)]
+ pub struct TreeNode {
+   pub val: i32,
+   pub left: Option<Rc<RefCell<TreeNode>>>,
+   pub right: Option<Rc<RefCell<TreeNode>>>,
+ }
+ 
+ impl TreeNode {
+   #[inline]
+   pub fn new(val: i32) -> Self {
+     TreeNode {
+       val,
+       left: None,
+       right: None
+     }
+   }
+ }
+use std::rc::Rc;
+use std::cell::RefCell;
+pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    match root {
+        Some(rc) => {
+            let node = rc.borrow();
+            let val = node.val;
+
+            let mut left = postorder_traversal(node.left.clone());
+            let mut right = postorder_traversal(node.right.clone());
+
+            left.extend(right);
+            left.push(val);
+            left
+        }
+        None => {
+            vec![]
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn test_postorder_traversal() {
+        let left = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+        let right = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+        let root = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: left,
+            right: right,
+        })));
+        
+        assert_eq!(postorder_traversal(root), vec![1, 3, 2]);
+    }
+}
